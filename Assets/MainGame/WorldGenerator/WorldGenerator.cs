bcae562c9be3455b1ce44a0359bf03ad90f7fe61
Daryl Valairan <darylvalairan@outlook.com>
2026-01-09 20:52:20 -0500

    Simplified world generation

--- Assets/MainGame/WorldGenerator/WorldGenerator.cs
+++ Assets/MainGame/WorldGenerator/WorldGenerator.cs
@@ -2,29 +2,24 @@ using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
 {
-    [SerializeField] AnimationCurve heightCurve;
-    [SerializeField] int mapSizeX;
-    [SerializeField] int mapSizeY;
-    [SerializeField] float scale;
-    [SerializeField] float persistance;
-    [SerializeField] float lacunarity;
-    [SerializeField] Vector2 offset;
 
+    float[,] terrainNoise;
+    float[,] biomeNoise;
+    float[,] weatherNoise;
+    Vector3 cubeSize = new Vector3(0.2f, 0.2f, 0.2f);
     void Start()
     {
-        int biomeSeed = Random.Range(0, 100);
-        int terrainSeed = Random.Range(0, 100);
-        int weatherSeed = Random.Range(0, 100);
+        terrainNoise = NoiseGenerator.GenerateNoiseMap(200, 100, 10, 12, 4, 0f, 16f, Vector2.zero);
+        biomeNoise = NoiseGenerator.GenerateNoiseMap(10, 10, 10, 4, 4, 16f, 16f, Vector2.zero);
+        weatherNoise = NoiseGenerator.GenerateNoiseMap(100, 100, 10, 4, 4, 16f, 16f, Vector2.zero);
 
-        //send the above values to the server
+        GameObject terrain = MeshGenerator.GenerateTerrainMesh(100, 100, 5f, terrainNoise, 10, 10);
+        terrain.transform.localScale = new Vector3(15, 15, 15);
+        terrain.transform.position = new Vector3(-750, 0, -750);
 
+        GameObject ocean = MeshGenerator.GenerateSquareMesh(20, 20, 30);
+        ocean.transform.localScale = new Vector3(80, 1, 80);
+        ocean.transform.position = new Vector3(-750, 0, -750);
 
-        //generate perlin noise using seeds
-        float[,] biomeNoise = MeshGenerator.GenerateNoiseMap(100, 100, biomeSeed, 100, 4, 4, 4, Vector2.zero);
-        float[,] terrainNoise = MeshGenerator.GenerateNoiseMap(100, 100, terrainSeed, 100, 4, 4, 4, Vector2.zero);
-        float[,] weatherNoise = MeshGenerator.GenerateNoiseMap(100, 100, weatherSeed, 100, 4, 4, 4, Vector2.zero);
-
-        GameObject terrain = MeshGenerator.GenerateTerrainGrid(100, 100, terrainNoise, 2, heightCurve);
-        terrain.transform.SetParent(transform);
     }
 }

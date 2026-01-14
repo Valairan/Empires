e7ff6d17d7a6c1f678052ce6ab343e4e6becc6d4
Daryl Valairan <darylvalairan@outlook.com>
2026-01-14 04:35:12 -0500

    Started working on player controller

---
+++ Assets/MainGame/Player/InputController.cs
@@ -0,0 +1,10 @@
+
+using System;
+using TMPro;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    
+
+}
--- Assets/MainGame/Player/PlayerController.cs
+++ Assets/MainGame/Player/PlayerController.cs
@@ -1,7 +1,32 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    [Header("Components")]
+    [SerializeField] CharacterController playerCCMotor;
+
+    [Header("Locomotion Settings")]
+    [SerializeField] float runSpeed;
+    [SerializeField] float walkSpeed;
+
+    [Header("Camera Settings")]
+    [SerializeField] Camera playerCamera;
+    [SerializeField] Transform playerCameraParent;
+    [SerializeField] Transform playerCameraOrbit;
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerCCMotor.Move(transform.forward);
+        }
+    }
+
+    public void LateUpdate()
+    {
+
+    }
+
 }
